579138a352e4bf7577eaa18e4e629f6ca1e60990
Surendran Balachandran <surendran@Surendrans-MacBook-Air.local>
2026-02-28 16:01:51 +0530

    chore: version bump to v1.0.0 (Official Stable Release)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fuseforge.cardash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6.0"
+        versionCode = 8
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
