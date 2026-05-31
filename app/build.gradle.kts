75e365be91ab5c33db402da2c36037f786d910e5
Surendran Balachandran <surendran@Surendrans-MacBook-Air.local>
2026-05-31 09:45:41 +0530

    chore: bump version to 2.3.8 (code 43) for Play Console upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fuseforge.cardash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 42
-        versionName = "2.3.7"
+        versionCode = 43
+        versionName = "2.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
